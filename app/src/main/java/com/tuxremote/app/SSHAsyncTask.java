a9c63e8c2559de3667b5db5af4753669589beaf3
lheido <lheido.web@gmail.com>
2014-06-04 11:20:13 +0200

    add SSHAsyncTask class structure

---
+++ app/src/main/java/com/tuxremote/app/SSHAsyncTask.java
@@ -0,0 +1,56 @@
+package com.tuxremote.app;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by lheido on 04/06/14.
+ */
+public class SSHAsyncTask extends AsyncTask<Void, Void, Boolean> {
+
+    private final Command cmd;
+    private WeakReference<MainActivity> act;
+    private Context context;
+
+    public SSHAsyncTask(MainActivity activity, Command cmd){
+        link(activity);
+        this.cmd = cmd;
+    }
+
+    private void link(MainActivity activity) {
+        act = new WeakReference<MainActivity>(activity);
+    }
+
+    @Override
+    protected void onPreExecute () {
+        if(act.get() != null){
+            context = act.get().getApplicationContext();
+        }
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... voids) {
+        // isCancelled() //doc : the value returned by this method should be checked periodically here
+        // call onProgressUpdate to publish updates on the UI thread.
+        //publishProgress();
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate (Void... prog) {
+        if (act.get() != null){
+            //act.get().updateProgress(prog[0]);
+        }
+    }
+
+    @Override
+    protected void onPostExecute (Boolean result) {
+        if (act.get() != null) {
+            if(!result){
+                // échec :(
+            }
+        }
+    }
+}
