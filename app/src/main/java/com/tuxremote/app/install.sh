8dbd806fcab1e913613c9ed34beca9f99613636b
hellmak <makhloufmehdi1@gmail.com>
2014-08-04 22:50:43 +0200

    myOwnSudo for TuxRemote

--- app/src/main/java/com/tuxremote/app/install.sh
+++ app/src/main/java/com/tuxremote/app/install.sh
@@ -15,4 +15,5 @@ if [ -f $config ]
     echo "création du fichier $config"
     echo "plop" > $config
 fi
+cp /sbin/shutdown $HOME/.config/TuxeRemote/TuxeRemote-shutdown
 sudo echo "$USER ALL = NOPASSWD : /home/$USER/.config/TuxRemote/TuxeRemote-shutdown" > etc/sudoers
